1d5f2ab27533aeb291c56242f8be77656c356533
Amila Ruwan Guruge <amilaruwanguruge@gmail.com>
2023-05-21 23:25:44 +0200

    Delete Database directory

--- Database/AlarmPriority.sql
+++
@@ -1,8 +0,0 @@
-insert into [ALARM_PRIORITIES] (Description, Deviation)
-values ('High',0.13)
-
-insert into [ALARM_PRIORITIES] (Description, Deviation)
-values ('Medium',0.1)
-
-insert into [ALARM_PRIORITIES] (Description, Deviation)
-values ('Low',0.07)
