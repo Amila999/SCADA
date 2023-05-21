c9a8b924b5fdb63decb5d8edb983e2ea8fef57d1
Amila Ruwan Guruge <amilaruwanguruge@gmail.com>
2023-05-21 23:27:39 +0200

    Add files via upload

---
+++ Database/AlarmPriority.sql
@@ -0,0 +1,8 @@
+insert into [ALARM_PRIORITIES] (Description, Deviation)
+values ('High',0.13)
+
+insert into [ALARM_PRIORITIES] (Description, Deviation)
+values ('Medium',0.1)
+
+insert into [ALARM_PRIORITIES] (Description, Deviation)
+values ('Low',0.07)
